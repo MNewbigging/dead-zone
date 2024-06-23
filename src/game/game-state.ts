dab497085478e9938c62395457c5ed7031739ab3
Matt Newbigging <mrnewbigging@gmail.com>
2024-06-23 17:11:46 +0100

    basic movement

--- src/game/game-state.ts
+++ src/game/game-state.ts
@@ -6,6 +6,7 @@ import { RenderPipeline } from "./render-pipeline";
 import { AnimatedCharacter } from "./animated-character";
 import { EventListener } from "../listeners/event-listener";
 import { MouseListener } from "../listeners/mouse-listener";
+import { KeyboardListener } from "../listeners/keyboard-listener";
 
 export class GameState {
   private renderPipeline: RenderPipeline;
@@ -16,6 +17,7 @@ export class GameState {
   private controls: PointerLockControls;
 
   private mouseListener = new MouseListener();
+  private keyboardListener = new KeyboardListener();
 
   // private animatedCharacter: AnimatedCharacter;
 
@@ -101,6 +103,19 @@ export class GameState {
     );
     floor.rotateX(-Math.PI / 2);
     this.scene.add(floor);
+
+    const boxMat = new THREE.MeshBasicMaterial({ color: "red" });
+    const box1 = new THREE.Mesh(new THREE.BoxGeometry(), boxMat);
+    box1.position.set(0, 1, -1);
+    this.scene.add(box1);
+
+    const box2 = new THREE.Mesh(new THREE.BoxGeometry(), boxMat);
+    box2.position.set(2, 1, 1);
+    this.scene.add(box2);
+
+    const box3 = new THREE.Mesh(new THREE.BoxGeometry(), boxMat);
+    box3.position.set(-3, 1, -1);
+    this.scene.add(box3);
   }
 
   // private setupAnimatedCharacter(): AnimatedCharacter {
@@ -124,6 +139,27 @@ export class GameState {
 
     const dt = this.clock.getDelta();
 
+    this.playerMovement(dt);
+
     this.renderPipeline.render(dt);
   };
+
+  private playerMovement(dt: number) {
+    const moveForward = this.keyboardListener.isKeyPressed("w");
+    const moveBackward = this.keyboardListener.isKeyPressed("s");
+    const moveLeft = this.keyboardListener.isKeyPressed("a");
+    const moveRight = this.keyboardListener.isKeyPressed("d");
+
+    const moveDirection = new THREE.Vector3();
+    moveDirection.z = Number(moveForward) - Number(moveBackward);
+    moveDirection.x = Number(moveRight) - Number(moveLeft);
+    moveDirection.normalize();
+
+    const velocity = new THREE.Vector3();
+    velocity.z = moveDirection.z * dt * 5;
+    velocity.x = moveDirection.x * dt * 5;
+
+    this.controls.moveForward(velocity.z);
+    this.controls.moveRight(velocity.x);
+  }
 }
